a614b5cbafafaf57ffdd46dbe308237b0a7c33b7
Mark Volosiuk <marchi.martius@gmail.com>
2017-11-29 13:33:15 +0200

    Store model attributes with `msgctxt`

--- lib/gettext_i18n_rails/model_attributes_finder.rb
+++ lib/gettext_i18n_rails/model_attributes_finder.rb
@@ -14,7 +14,7 @@ module GettextI18nRails
           #all columns namespaced under the model
           column_names.each do |attribute|
             translation = model.gettext_translation_for_attribute_name(attribute)
-            f.puts("_('#{translation}')")
+            f.puts("s_('#{translation}')")
           end
         end
         f.puts "#DO NOT MODIFY! AUTOMATICALLY GENERATED FILE!"
